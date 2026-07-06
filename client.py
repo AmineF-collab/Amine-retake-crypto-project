a9466382697cad1512cbaff424d8845217f88fbb
AmineF-collab <ferchichiamine2007@gmail.com>
2026-07-06 18:22:45 +0200

    Client se connecte au serveur et peut se deconnecter ENFINNNNN
    cli aussi finit mais pas compléter

--- client.py
+++ client.py
@@ -1,3 +1,52 @@
+import socket
+import server
+def client():
+    host = '127.0.0.1'
+    port = 6000
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Client started (not connected yet).\n Type /help to see available commands, or /connect to connect to the server.')
+    while True :
+        command = input("\n> ")
+        if command == "/help" :
+            print("/help")
+            print("/connect")
+            print("/disconnect")
+            print("/generate_keys <username>")
+            print("/send_text <username> <object_name> <message>")
+            print("/listaf")
+            print("/get <object_id>")
+            print("/verify <object_id>")
+            print("/verify_all")
+            print("/tamper <object_id>")
+            print("/exit")
+        if command == "/connect" :
+            try:
+                server.server1.connect
+                client.connect((host,port))
+                print("Connected to server 127.0.0.1:6000")
+            except OSError:
+                print("You are already connected")
+        
+        if command == "/disconnect" :
+            client.close()
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Disconnected from server")
+        if command == "/generate_keys <username>" :
+            pass
+        if command == "/send_text <username> <object_name> <message>":
+            pass
+        if command == "/listaf":
+            pass
+        if command == "/get <object_id>":
+            pass
+        if command == "/verify <object_id>":
+            pass
+        if command == "/verify_all":
+            pass
+        if command == "/tamper <object_id>":
+            pass
+        if command == "/exit":
+            pass
 
 print('Client started (not connected yet).\n Type /help to see available commands, or /connect to connect to the server.')
 command = input("\n> ")
@@ -16,3 +65,4 @@ if command == "/help" :
 
 
 
+client()
--- server.py
+++ server.py
@@ -1,15 +1,31 @@
 import socket
+host = '127.0.0.1'
+port = 6000
+print("Server started on 127.0.0.1:6000\nWaiting for clients...")
+
+connected = True   
+
+
 class Server:
     def __init__(self):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def connect(self, Ip_address, port):
-        self.client.connect(Ip_address,port)
-        print(f'Connected on {Ip_address}:{port}')
-
+        self.host = host
+        self.port = port
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    def start(self):
+        self.server.bind((self.host,port))  
+        self.server.listen()
+         
+    def connect(self):
+        while True:
+            client_arrive, addresse = self.server.accept
+            print(f"Connected with {addresse}")
+            client_arrive.send(f"T'es connecté au server".encode('ascii'))
+            print("normalement c'est sensé accepter")
+    
     def send_message(self, msg, type_msg):
         packet = (msg, type_msg)
-        self.client.sendall(packet)
+        self.server.sendall(packet)
         print('Message sent')
 
     def receive(self):
@@ -17,3 +33,7 @@ class Server:
     
     def close(self):
         self.client.close
+
+server1 = Server()
+server1.start()
+print("Serveur a start normalement")
