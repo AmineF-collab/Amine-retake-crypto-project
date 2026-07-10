62b36d5d42d1bc77855a59751154bfbc533df8b8
AmineF-collab <ferchichiamine2007@gmail.com>
2026-07-10 13:15:53 +0200

    Framing error corrected

--- protocol/frame.py
+++ protocol/frame.py
@@ -2,20 +2,22 @@ import socket
 import struct
 
 def send_msg(sock: socket.socket, data: bytes) -> None:
-    header = struct.pack(">H", 3)
-    type = struct.pack(">H", 1)
-    length = struct.pack(">H", 4)
+    header = b"SFX"
+    type = struct.pack(">B", 1)
+    length = struct.pack(">I", 4)
     sock.sendall(header + type + length + data)
 
 def recv_msg(sock: socket.socket) -> bytes:
     raw_header = _recvn(sock, 3)
+    if raw_header != b"SFX":
+        raise ValueError("Mauvais header de frame")
     msg_len = struct.unpack(">I", raw_header)[0]
     raw_type = _recvn(sock, 1)
-    msg_len = struct.unpack(">I", raw_type)[0]
+    msg_len = struct.unpack(">B", raw_type)[0]
     raw_length = _recvn(sock, 4)
     msg_len = struct.unpack(">I", raw_length)[0]
-
-    return _recvn(sock, msg_len) + f"{str(raw_header)}, {str(raw_type)}, {str(raw_length)}"
+    payload = _recvn(sock, msg_len)
+    return msg_len, payload
 
 
 def _recvn(sock: socket.socket, n: int) -> bytes:
